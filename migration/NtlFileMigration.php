6ff825191a3589cca19de6961ac8286783f5af3d
Inigo San Gil <isangil@lternet.edu>
2014-04-01 09:46:53 -0500

    Improves the image migration

--- migration/NtlFileMigration.php
+++ migration/NtlFileMigration.php
@@ -10,8 +10,7 @@ class NtlFileMigration extends DeimsFileMigration {
   public function prepare($file, $row) {
     parent::prepare($file, $row);
 
-    // here we set customizations... not worked yet, this is a sev copy
-    // Add data for scientific images.
+    // Add data for  images.
     $connection = Database::getConnection('default', $this->sourceConnection);
     $query = $connection->select('node', 'n');
     $query->condition('n.type', 'image');
@@ -19,7 +18,6 @@ class NtlFileMigration extends DeimsFileMigration {
     $query->fields('nr', array('title', 'body', 'format'));
     $query->join('image', 'cti', 'n.nid = cti.nid');
     $query->condition('cti.fid', $row->fid);
-//    $query->fields('cti', array('field_image_author_nid'));
     $query->join('term_node', 'tn', 'tn.vid = nr.vid');
     $query->fields('tn', array('tid'));
     if ($result = $query->execute()->fetch()) {
@@ -29,11 +27,36 @@ class NtlFileMigration extends DeimsFileMigration {
       if (!empty($result->body)) {
         if ( preg_match('/Date: (\d+)/',$result->body, $found)){
           preg_replace('/Date: \d+/',' ',$result->body);
-          $mydate = date('Y-m-d H:i:s',strtotime("$found[1]".'-00-00T00:00:00')) ;  // may need lots of tweaking
+          $mydate = date('Y-m-d H:i:s',strtotime("$found[1]-01-01 00:00:00")) ;  // may need lots of tweaking
           $file->field_date[LANGUAGE_NONE][0]['value'] = $mydate; 
         } else if ( preg_match('/Date:(\w+)\s(\d+)/',$result->body,$found)){
-          $mydate = date('Y-m-d H:i:s',strtotime("$found[2]".'-00-00T00:00:00')) ;  // 
-          $file->field_date[LANGUAGE_NONE][0]['value'] =  $mydate;
+            switch($found[1]) {
+               case "Sept.":
+                 $mydate = date('Y-m-d H:i:s',strtotime("$found[2]".'-09-01 00:00:00')) ;  // 
+               case "Jul.":
+                 $mydate = date('Y-m-d H:i:s',strtotime("$found[2]".'-07-01 00:00:00')) ;  // 
+               case "Aug.":
+                 $mydate = date('Y-m-d H:i:s',strtotime("$found[2]".'-08-01 00:00:00')) ;  // 
+               case "Jun.":
+                 $mydate = date('Y-m-d H:i:s',strtotime("$found[2]".'-06-01 00:00:00')) ;  // 
+               case "January":
+                 $mydate = date('Y-m-d H:i:s',strtotime("$found[2]".'-01-01 00:00:00')) ;  // 
+               case "June":
+                 $mydate = date('Y-m-d H:i:s',strtotime("$found[2]".'-06-01 00:00:00')) ;  // 
+               case "Aug":
+                 $mydate = date('Y-m-d H:i:s',strtotime("$found[2]".'-08-01 00:00:00')) ;  // 
+               case "July":
+                 $mydate = date('Y-m-d H:i:s',strtotime("$found[2]".'-07-01 00:00:00')) ;  // 
+               case "Summer":
+                 $mydate = date('Y-m-d H:i:s',strtotime("$found[2]".'-07-01 00:00:00')) ;  // 
+               case "Fall":
+                 $mydate = date('Y-m-d H:i:s',strtotime("$found[2]".'-10-01 00:00:00')) ;  // 
+               case "Winter":
+                 $mydate = date('Y-m-d H:i:s',strtotime("$found[2]".'-01-01 00:00:00')) ;  // 
+               case "Spring":
+                 $mydate = date('Y-m-d H:i:s',strtotime("$found[2]".'-05-01 00:00:00')) ;  // 
+            }
+            $file->field_date[LANGUAGE_NONE][0]['value'] =  $mydate;
         }
 //       Circulat dependency: THis migration needs Person, but Person need images....
 //        if ( preg_match('/Photographer: (\w+)\s(\w+)/',$result->body, $found)){
@@ -48,7 +71,7 @@ class NtlFileMigration extends DeimsFileMigration {
         $file->field_photo_caption[LANGUAGE_NONE][0] = array('value' => $result->body, 'format' => $this->mapFormat($result->format));
 //        $file->field_related_people[LANGUAGE_NONE][0] = array('target_id' => $result->photographer_person);
         if ($result->newtid = $this->handleSourceMigration('NtlTaxonomyImageGalleries', $result->tid)) {
-          $file->field_download_keywords[LANGUAGE_NONE][0] = array('tid' => $result->newtid );
+          $file->field_term_im_galleries[LANGUAGE_NONE][0] = array('tid' => $result->newtid );
         }
       }
     }
